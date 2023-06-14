5a608311c6e6516968f3d981cdf04156a73612b8
Jerome Laban <jerome.laban@nventive.com>
2023-06-14 11:54:00 -0400

    ci: Adjust WinUI build

--- src/BehaviorsSDKManaged/Microsoft.Xaml.Interactivity.Shared/ResourceHelper.cs
+++ src/BehaviorsSDKManaged/Microsoft.Xaml.Interactivity.Shared/ResourceHelper.cs
@@ -6,13 +6,13 @@ namespace Microsoft.Xaml.Interactivity
 
     internal static class ResourceHelper
     {
-#if NET5_0_WINDOWS10_0_18362_0
+#if WinUI && !HAS_UNO
         private static ResourceLoader strings = new ResourceLoader(ResourceLoader.GetDefaultResourceFilePath(), "Microsoft.Xaml.Interactivity/Strings");
 #endif
 
         public static string GetString(string resourceName)
         {
-#if !NET5_0_WINDOWS10_0_18362_0
+#if !WinUI || HAS_UNO
             ResourceLoader strings = ResourceLoader.GetForCurrentView("Microsoft.Xaml.Interactivity/Strings");
 #endif
             return strings.GetString(resourceName);
